598c4b70ea437be3dc7b51139bfeb0233c426700
Theo Olsson <theo.olsson1@learnet.se>
2022-04-22 13:58:18 +0200

    Add project files.

---
+++ RobberLanguageAPI/Controllers/TranslationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RobberLanguageAPI.Model;
+
+namespace RobberLanguageAPI.Controllers
+{
+    [Route("api/RobberLanguage")]
+    [ApiController]
+    public class TranslationController : ControllerBase
+    {
+        [Route("CreateTranslation")]
+        [HttpPost]
+        public Translation Translate(string sentence)
+        {
+            return new Translation()
+            {
+                OriginalSentence = sentence,
+                TranslatedSentence = TranslateSentence(sentence),
+            };
+        }
+
+        private static string TranslateSentence(string sentence)
+        {
+            const string consonants = "bcdfghjklmnpqrstvwxz";
+            string newSentence = sentence;
+
+            foreach (var consonant in consonants)
+            {
+                string c = Convert.ToString(consonant);
+                newSentence = newSentence.Replace(c, $"{c}o{c}");
+            }
+
+            return newSentence;
+        }
+    }
+}
---
+++ RobberLanguageAPI/Models/Translation.cs
@@ -0,0 +1,8 @@
+﻿namespace RobberLanguageAPI.Model
+{
+    public class Translation
+    {
+        public string OriginalSentence { get; set; }
+        public string TranslatedSentence { get; set; }
+    }
+}
